b1d09fa05402ed58408ce022f7eda072b400dac3
Magdalena Szula-Chraplewska <magdalenaszula@gmail.com>
2017-10-11 22:04:56 +0200

    javascript chessboard added

--- szachownica.js
+++ szachownica.js
@@ -19,5 +19,28 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		return matrix;
 	}
 
-	console.log('')
+	var matrix = generateTable()
+	var container = ""
+
+	for(var y=0; y<8; y++) {
+		var row = matrix[y]
+		var row_html = "<div class='row'>"
+
+		for(var x=0;x<8;x++) {
+			var el = row[x]
+		  var el_html;
+
+			if (el) {
+				el_html = "<div class='boxW'></div>"
+			} else {
+				el_html = "<div class='boxB'></div>"
+			}
+			row_html += el_html
+		}
+
+		row_html += '<div style="clear:both;"></div></div>'
+		container += row_html
+	}
+
+	document.getElementById("szachownica").innerHTML = container
 });
