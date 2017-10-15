9776f29cc9d00a51501c3baa0683357758b750be
Magdalena Szula-Chraplewska <magdalenaszula@gmail.com>
2017-10-15 11:47:31 +0200

    document.write removed from calculate function

--- funkcja_kwadratowa.js
+++ funkcja_kwadratowa.js
@@ -1,24 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(e) {
   function calculate(a, b, c) {
-    document.write("Parametry równania: <br />");
-    document.write("a = "+ a + ", b = " + b +", c = " + c +"  <br />");
+    //document.write("Parametry równania: <br />");
+    //document.write("a = "+ a + ", b = " + b +", c = " + c +"  <br />");
  
     if (a == 0) {
-      document.write("To nie jest równanie kwadratowe: a = 0!");
+      var an = "To nie jest równanie kwadratowe: a = 0!"
+      document.getElementById("demo").innerHTML = an
+   
     } else {
       var delta = b * b - 4 * a * c
 
       if (delta < 0) {
-        document.write ("Delta < 0 <br/>");
-        document.write ("To równanie nie ma rozwiązania w zbiorze liczb rzeczywistych!");
+        var dn1 = "Delta < 0. To równanie nie ma rozwiązania w zbiorze liczb rzeczywistych!"
+        document.getElementById("demo").innerHTML = dn1
+        
       } else if (delta == 0) {
         var wynik = - b / 2 * a;
-        document.write("Rozwiązanie: x = " + wynik + "");
+        var wn = "Rozwiązanie: x = " + wynik + ""
+        document.getElementById("demo").innerHTML = wn 
       } else {
-        wynik1 = (- b + Math.sqrt(delta)) / 2 * a;
-        document.write ("Rozwiązanie: x1 = " + wynik1.toFixed(2));
-        wynik2 = (- b - Math.sqrt(delta)) / 2 * a;
-        document.write(",  x2 = " + wynik2.toFixed(2));
+        var wynik1 = (- b + Math.sqrt(delta)) / 2 * a;
+        var wn1 = "Rozwiązanie: x1 = " + wynik1.toFixed(2);
+               
+        var wynik2 = (- b - Math.sqrt(delta)) / 2 * a;
+        var wn2 = "Rozwiązanie: x2 = " + wynik1.toFixed(2);
+        document.getElementById("demo").innerHTML = wn1 + wn2;
       }
 
     }
